01f3abcdc52129145afe22badff745d2127edfc1
BobbyShmurner <glitched.ohdeymus@gmail.com>
2022-12-08 22:51:30 +0000

    Complete Day 3 Part 1

--- src/year_2022/day_3.rs
+++ src/year_2022/day_3.rs
@@ -2,10 +2,52 @@ use simple_error::SimpleError;
 
 use crate::DayReturnType;
 
-pub fn execute(_input: &str) -> DayReturnType {
-    Err(Box::new(SimpleError::new(
-        "Code For This Day Is Not Complete!",
-    )))
+fn get_common_char(input: &str) -> Option<char> {
+    let (first_half, second_half) = input.trim().split_at(input.len() / 2);
+
+    first_half
+        .chars()
+        .find(|&letter| second_half.contains(letter))
+}
+
+fn get_char_priority(letter: &char) -> u32 {
+    let mut buffer = [0; 1];
+    letter.encode_utf8(&mut buffer);
+
+    let byte = buffer[0];
+
+    if letter.is_uppercase() {
+        (byte - 38) as u32
+    } else {
+        (byte - 96) as u32
+    }
+}
+
+fn get_line_priority(line: &str) -> Result<u32, String> {
+    let common_char = match get_common_char(line) {
+        Some(val) => val,
+        None => {
+            return Err(format!(
+                "Failed to find a common character in the line \"{}\"",
+                line
+            ))
+        }
+    };
+
+    Ok(get_char_priority(&common_char))
+}
+
+pub fn execute(input: &str) -> DayReturnType {
+    let mut total_priority = 0;
+
+    for line in input.trim().lines() {
+        total_priority += match get_line_priority(line) {
+            Ok(val) => val,
+            Err(e) => return Err(Box::new(SimpleError::new(e))),
+        }
+    }
+
+    Ok((total_priority.to_string(), "Answer2".to_string()))
 }
 
 #[cfg(test)]
