148bc1e9e273e4ccdb75a6a960efe39fe5f2e1ef
BobbyShmurner <glitched.ohdeymus@gmail.com>
2022-12-13 21:59:47 +0000

    Program now loops instead of exiting on completion

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,20 @@ extern crate advent_of_code;
 use advent_of_code::pause;
 use clearscreen::clear;
 
-use std::process;
-
 fn main() {
-    clear().unwrap();
-    println!("Welcome To Bobby Shmurner's Advent Of Code!");
+    loop {
+        clear().unwrap();
+        println!("Welcome To Bobby Shmurner's Advent Of Code!");
 
-    match advent_of_code::select_day() {
-        Ok((answer_1, answer_2)) => {
-            println!("Complete!\n\nPart 1: {answer_1}\nPart 2: {answer_2}");
-        }
-        Err(error) => {
-            eprintln!("An error occured: {error}");
-            process::exit(1);
-        }
-    };
+        match advent_of_code::select_day() {
+            Ok((answer_1, answer_2)) => {
+                println!("Complete!\n\nPart 1: {answer_1}\nPart 2: {answer_2}");
+            }
+            Err(error) => {
+                eprintln!("An error occured: {error}");
+            }
+        };
 
-    pause("\nPress Enter To Exit...");
+        pause("\nPress Enter To Continue...");
+    }
 }
